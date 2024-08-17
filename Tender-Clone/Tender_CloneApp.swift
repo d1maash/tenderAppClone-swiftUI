aa3f946c6049742f2b6fa505604c830fea24ac96
Dimash <dimashaijarikov@gmail.com>
2024-08-17 19:35:06 +0500

    Initial Commit

---
+++ Tender-Clone/Tender_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tender_CloneApp.swift
+//  Tender-Clone
+//
+//  Created by Динмуханбет Айжарыков on 17.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Tender_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
