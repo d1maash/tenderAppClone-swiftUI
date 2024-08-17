ab5ac5006192a8e23140e47f7a8b2622a9a71407
Dimash <dimashaijarikov@gmail.com>
2024-08-17 22:14:01 +0500

    creating st data

---
+++ Tender-Clone/Shared/Users/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Tender-Clone
+//
+//  Created by Динмуханбет Айжарыков on 17.08.2024.
+//
+
+import Foundation
+
+struct User: Identifiable {
+    let id: String
+    let fullanme: String
+    var age: Int
+    var profileImageUrls: (String)
+}
