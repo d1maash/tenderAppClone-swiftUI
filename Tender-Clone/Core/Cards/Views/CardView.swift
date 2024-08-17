877767d4d7bb3c598f1190a0ca45df70d3ebe365
Dimash <dimashaijarikov@gmail.com>
2024-08-17 20:06:38 +0500

    creating cards

--- Tender-Clone/Core/Cards/Views/CardView.swift
+++ Tender-Clone/Core/Cards/Views/CardView.swift
@@ -8,8 +8,25 @@
 import SwiftUI
 
 struct CardView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottom) {
+            Image(.firstPerson)
+                .resizable()
+                .scaledToFill()
+                .frame(width: cardWidth, height: cardHeight)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+        }
+    }
+}
+
+
+private extension CardView {
+    private var cardWidth: CGFloat {
+        UIScreen.main.bounds.width - 20
+    }
+    private var cardHeight: CGFloat {
+        UIScreen.main.bounds.height / 1.45
     }
 }
 
