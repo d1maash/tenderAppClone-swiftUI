912cae06ced0d0faf48c28b4f1d5800ae13ddd9c
Dimash <dimashaijarikov@gmail.com>
2024-08-18 20:36:40 +0500

    swipe action

---
+++ Tender-Clone/Core/Cards/Model/SwipeAction.swift
@@ -0,0 +1,14 @@
+//
+//  SwipeAction.swift
+//  Tender-Clone
+//
+//  Created by Динмуханбет Айжарыков on 18.08.2024.
+//
+
+import Foundation
+
+
+enum SwipeAction {
+    case reject
+    case like 
+}
